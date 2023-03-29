5c663e1d96aa3e12747020760aefda02fe6bd2e5
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-03-29 23:18:29 +0530

    Adding Dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css';
+import React, { useState } from 'react';
 import Gallery from './Gallery';
+import './App.css';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
-    <div className="App">
+    <div className={isDarkMode ? 'dark' : 'light'}>
+      <div className='toggle-switch'>
+        <label className='toggle-switch-label'>
+          <input type="checkbox" checked={isDarkMode} onChange={handleToggle} />
+          <span className='icons'>{isDarkMode ? <FaMoon /> : <FaSun />}</span>
+        </label>
+      </div>
       <h1 className="title">Midjourney Gallery</h1>
       <Gallery />
       <footer>
