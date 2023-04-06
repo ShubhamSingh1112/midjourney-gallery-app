cec44b594b1e1b3c59d382aeaeda3228674970a4
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-04-06 22:16:13 +0530

    Adding Loading placeholder to avoid CLS

--- src/GalleryItem.jsx
+++ src/GalleryItem.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function GalleryItem({ item }) {
   const { title, imageUrl } = item;
+  const [loaded, setLoaded] = useState(false);
 
+  const handleLoad = () => {
+    setLoaded(true);
+  };
+  
   return (
     <div className="gallery-item">
       <a data-fancybox="gallery" data-caption={title} href={imageUrl}>
-        <img src={imageUrl} alt={title}  />
+        {!loaded && <div className="shimmer"></div>}
+        <img
+          src={imageUrl}
+          alt={title}
+          onLoad={handleLoad}
+          style={loaded ? {} : { display: 'none' }}
+        />
         <h3>{title}</h3>
       </a>
     </div>
